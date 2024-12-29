68d22a7c02cb202054ee89b892d603b4e53110d1
devsahal-crystallite <dev.ahs@productions-dev.com>
2024-12-30 02:59:32 +0500

    fixing in cta

--- resources/views/components/form-c-t-a.blade.php
+++ resources/views/components/form-c-t-a.blade.php
@@ -1,4 +1,3 @@
-
 <section class=" d-lg-block d-md-block " style="margin-top:3%;margin-bottom:8%">
     <div class="container-fluid">
         <h2 class="font-secondary fw-900 font-50 font-50rem text-blue text-center"><span class="extraColor">Professional
@@ -11,31 +10,32 @@
                 <div class="bg-brownColor form-cta">
                     <form action="" class="p-0 m-0 myctaform">
                         <div class="row">
-                            <div class="col-3">
-
-                                <input type="text" class="form-control input-h-12" placeholder="Enter Good Name">
-
+                            <div class="col-lg-3 col-md-6">
+                                <div class="mb-3">
+                                    <input type="text" class="form-control input-h-12" placeholder="Enter Good Name">
+                                </div>
                             </div>
-                            <div class="col-3">
-
-                                <input type="text" class="form-control input-h-12" placeholder="Enter Email Address">
-
+                            <div class="col-lg-3 col-md-6">
+                                <div class="mb-3">
+                                    <input type="text" class="form-control input-h-12" placeholder="Enter Email Address">
+                                </div>
                             </div>
-                            <div class="col-3">
-
-                                <input type="text" class="form-control input-h-12" placeholder="Enter Phone Number">
-
+                            <div class="col-lg-3 col-md-6">
+                                <div class="mb-3">
+                                    <input type="text" class="form-control input-h-12" placeholder="Enter Phone Number">
+                                </div>
                             </div>
-                            <div class="col-3">
-
-                                <button type="submit" class="btn btn-secondary w-100 mybtn">
-                                    Let's Work Together
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="mybtnanimation" width="16" height="16"
-                                        fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
-                                        <path
-                                            d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
-                                    </svg>
-                                </button>
+                            <div class="col-lg-3 col-md-6">
+                                <div class="mb-3">
+                                    <button type="submit" class="btn btn-secondary w-100 mybtn">
+                                        Let's Work Together
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="mybtnanimation" width="16" height="16"
+                                            fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+                                            <path
+                                                d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
+                                        </svg>
+                                    </button>
+                                </div>
                             </div>
                         </div>
 
